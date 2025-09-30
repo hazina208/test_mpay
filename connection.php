a7fbd75101dcbba5c64afa8ee0565407f82e08c2
Makau Nganda <prosperhazina@gmail.com>
2025-09-30 17:34:16 +0300

    db_port in the file

--- connection.php
+++ connection.php
@@ -5,8 +5,9 @@ $host = getenv("bg4ikv5j4exzavzvcmwb-mysql.services.clever-cloud.com");
 $db   = getenv("bg4ikv5j4exzavzvcmwb");
 $user = getenv("uo0yxrgvekb7yhnz");
 $pass = getenv("lL6TCCUmkmY9oCsTEYsX");
+$port = getenv("3306");
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
