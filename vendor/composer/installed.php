01eeb7cdcd13d9813281e940fee72c9ce4e257e2
Makau Nganda <prosperhazina@gmail.com>
2025-09-30 10:16:04 +0300

    generated files after composer install command

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'myapp/php-app',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '358ea31724b2e3b46a4b162be29636a81238dfaa',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myapp/php-app' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '358ea31724b2e3b46a4b162be29636a81238dfaa',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
