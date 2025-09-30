b086fd2a241ad99e6ed66ddb2a17a5cdac82fb5d
Makau Nganda <prosperhazina@gmail.com>
2025-09-30 11:26:01 +0300

    changes to dd connection and docker file

--- DB_connection.php
+++ DB_connection.php
@@ -1,19 +1,4 @@
 <?php  
-
-$sName = "localhost";
-$uName = "root";
-$pass  = "";
-$db_name = "m_pay";
-
-try {
-	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOExeption $e){
-	echo "Connection failed: ". $e->getMessage();
-	exit;
-}
-
-
 $host = getenv('bg4ikv5j4exzavzvcmwb-mysql.services.clever-cloud.com');
 //$port = getenv('DB_PORT') ?: '3306';
 $dbname = getenv('bg4ikv5j4exzavzvcmwb');
--- dockerfile
+++ dockerfile
@@ -4,11 +4,12 @@ FROM php:8.2-apache
 # Enable extensions (mysqli/pdo_mysql for MySQL support)
 RUN docker-php-ext-install mysqli pdo pdo_mysql
 
+
 # Copy your PHP code into container
 COPY . /var/www/html/
 
 # Set working directory
 WORKDIR /var/www/html/
 
-# Install Composer
-COPY --from=composer:2 /usr/bin/composer /usr/bin/composer
+
+
