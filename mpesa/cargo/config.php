7d9899ceadffb1cbc73e53b923919304fb0e04d2
Makau Nganda <prosperhazina@gmail.com>
2026-01-13 16:29:42 +0300

    Update config.php

--- mpesa/cargo/config.php
+++ mpesa/cargo/config.php
@@ -14,6 +14,6 @@ define('MPESA_CALLBACK_URL_PAYBILL',    'https://test-mpay.onrender.com/mpesa/ca
 define('CALLBACK_URL_CARGO_MPESATOBANK','https://test-mpay.onrender.com/mpesa/cargo/callbackmpesatobank.php');
 
 define('INITIATE_URL', 'https://test-mpay.onrender.com/initiate.php');
-define('INTASEND_API_KEY', getenv('INTASEND_API_KEY') ?: 'ISSecretKey_test_xxxxxxxx');
+define('INTASEND_API_KEY',  getenv('INTASEND_API_KEY') ?: 'ISSecretKey_test_40c858c5-0aa8-4543-92b4-c7ba0bd38a0c'); // Wrapper for PesaLink
 
 date_default_timezone_set('Africa/Nairobi');
